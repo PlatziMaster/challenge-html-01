3dd212d0435a7478a41f00f85cfc7bd44d2a9b47
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-02-17 16:15:42 -0500

    the burger menu is commented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import '../styles/components/Header.styl';
 
 function Header() {
-  return <div />;
+  return (
+    <div>
+      <figure>
+        <img src="" alt=""/>
+      </figure>
+      <nav>
+        <ol>
+          <li><a href="">TECHNOLOGY</a></li>
+          <li><a href="">IDEAS</a></li>
+          <li><a href="">LEADERSHIP</a></li>
+          <li><a href="">VIDEO</a></li>
+          <li><a href="">NEWS</a></li>
+          <li><a href="">FINANCE</a></li>
+          <li><a href="">ENTERTAIMENT</a></li>
+        </ol>
+      </nav>
+      <span class="icon-menu"></span>
+
+    </div>
+  )
 }
 
 export default Header;
