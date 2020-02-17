b68e489111a7afbf380210f2933016fcab95efd3
David Behar <david@behar.io>
2020-02-17 13:29:10 -0600

    forEach products.json print ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 import '../styles/components/ProductCard.styl';
 
 function ProductCard({ product }) {
-  return <div>{product.title}</div>;
+  return (
+    <div className="ProductCard">
+      <figure className="ProductCard__imageFigure">
+        <img className="ProductCard__image" src={product.imgSrc} alt="" />
+      </figure>
+      {product.tags.map((tag) => (
+        <span className="ProductCard__tag">{tag}</span>
+      ))}
+      <h1 className="ProductCard__title">{product.title}</h1>
+      <p className="ProductCard__subtitle">{product.subtitle}</p>
+    </div>
+  );
 }
 
 export default ProductCard;
