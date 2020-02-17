ab872cd0ea115e3d2f247273199172607b77d45e
cristobalvega <cristobalvega96@gmail.com>
2020-02-17 13:39:44 -0600

    Se agregan las imagenes al componente principal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import '../styles/components/App.styl';
 
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return (
+    <div className="container">
+      <div className="item level-1">
+        <img src="../../resources/imgs/aidan-hancock-455440.png" alt="img" />
+      </div>
+      <div className="item level-1">
+        <img src="../../resources/imgs/annie-spratt-213059.png" alt="img" />
+      </div>
+      <div className="item level-1">
+        <img src="../../resources/imgs/crew-22235.png" alt="img" />
+      </div>
+      <div className="item level-1">
+        <img src="../../resources/imgs/dose-media-344938.png" alt="img" />
+      </div>
+      <div className="item level-1">
+        <img src="../../resources/imgs/jeff-sheldon-264920.png" alt="img" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
