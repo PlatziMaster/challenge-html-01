57074593eeaac810fb77cc6e32077db621ecc0b2
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-02-17 19:21:52 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import Nav from './Nav';
 import '../styles/components/Footer.styl';
 
 function Footer() {
-  return <div />;
+  return (
+    <div className="Footer">
+      <figure className="Footer__logoFigure">
+        <img className="Footer__logo" src="" alt="" />
+      </figure>
+      <div className="Footer__copyleft">
+        <span className="">© 2018 Deck</span>
+        <span>Component based UI Kit</span>
+        <div> </div>
+        <form action="/subscribe/" className="form-email">
+          <input type="text" placeholder="Your E-mail" id="email" />
+          <button type="button">SUBSCRIBE</button>
+        </form>
+        <div className="">
+          <span> Follow us:</span>
+          <div>
+
+          </div>
+        </div>
+
+      </div>
+      <Nav />
+    </div>
+  );
 }
 
 export default Footer;
