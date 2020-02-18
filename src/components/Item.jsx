62f5b4f7cc62f73f8b5a6166fdb166c5a23bba43
Jose Luis Campos Bautista <bautistaj20@hotmail.com>
2020-02-18 15:20:44 -0600

    Add template.

---
+++ src/components/Item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Item = () => <h1>Item</h1>;
+
+export default Item;
---
+++ src/containers/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Item from '../components/Item';
+
+const Home = () => (
+  <div className="App">
+    <Item />
+  </div>
+);
+
+export default Home;
