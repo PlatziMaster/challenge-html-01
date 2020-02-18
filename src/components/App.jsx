1176fbd59eba8faa0bb0ac897f2f0ec451e57c93
Alexis Araujo <ala1289@gmail.com>
2020-02-18 09:37:29 -0600

    Estructura base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 import '../styles/components/App.styl';
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => (
+  <div className="Main">
+    <Header />
+    <h1>Hello React!</h1>
+    <Footer />
+  </div>
+);
 
 export default App;
