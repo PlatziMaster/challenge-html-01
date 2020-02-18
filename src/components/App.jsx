38aa7388fd042d7fce8a1c97cde191e60cf41b2a
Rulo_code <drew.lizcano@gmail.com>
2020-02-17 21:57:01 -0500

    basic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable no-unused-expressions */
 import React from 'react';
 import '../styles/components/App.styl';
+import Image from '../../resources/imgs/annie-spratt-213059.png';
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => {
+  return (
+    <div>
+      file-loader
+      <img src={Image} alt="photico" />
+    </div>
+  );
+};
 
 export default App;
