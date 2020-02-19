e2cf920072f02e4e6c6a70e4e9d5cfe3ed92145a
Omar Flores Grimontt <omar.flores@3dves.com>
2020-02-19 17:19:51 -0500

    Created header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './Card';
-import cards from '../db';
+import Header from './Header';
+import db from '../db';
 import '../styles/components/App.styl';
 
 const App = () => {
   return (
     <div>
-      {cards.map(({ id, ...card }) => (
+      <Header sections={db.sections} />
+      {db.cards.map(({ id, ...card }) => (
         <Card key={id} {...card} />
       ))}
     </div>
---
+++ src/icons/BurgerButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function BurgerButton() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      enableBackground="new 0 0 32 32"
+      viewBox="0 0 32 32"
+    >
+      <path d="M4 10h24a2 2 0 000-1H4a2 2 0 000 1zm24 4H4a2 2 0 000 1h24a2 2 0 000-1zm0 5H4a2 2 0 000 1h24a2 2 0 000-1z" />
+    </svg>
+  );
+}
+
+export default BurgerButton;
