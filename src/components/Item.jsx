b1852923378097431db218d7c8911bc3f16e7014
Solej77 <ing.josgalo@gmail.com>
2020-02-18 23:44:20 -0600

    Componente Item 80%

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import '../styles/components/Item.styl';
+import hero from '../../resources/imgs/aidan-hancock-455440.png';
 
-const Item = () => <h1>Item</h1>;
+const Item = () => {
+  return (
+    <div className="Item">
+      <div className="Item__hero">
+        <span className="Item__hero--tag">Technology</span>
+        <figure className="Item__hero--image">
+          <img src={hero} alt="" />
+        </figure>
+      </div>
+      <div className="Item__description">
+        <h2 className="Item__description--title">Titulo</h2>
+        <h3 className="Item__description--subtitle">Subitulo</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Item;
