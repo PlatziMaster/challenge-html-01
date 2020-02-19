053931f64af49774502ac255e2408b56eb0a5f55
Solej77 <ing.josgalo@gmail.com>
2020-02-19 16:16:08 -0600

    Levantanto el estado a nivel Home usando Hooks

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import '../styles/components/Item.styl';
-import hero from '../../resources/imgs/aidan-hancock-455440.png';
 
-const Item = () => {
+const Item = ({ hero, tag, title, subtitle }) => {
   return (
     <div className="Item">
       <div className="Item__hero">
-        <span className="Item__hero--tag">Technology</span>
+        <span className="Item__hero--tag">{tag}</span>
         <figure className="Item__hero--image">
           <img src={hero} alt="" />
         </figure>
       </div>
       <div className="Item__description">
-        <h2 className="Item__description--title">
-          Search Engine Optimization And Advertising
-        </h2>
-        <h3 className="Item__description--subtitle">
-          There us a lot of exciting stuff going on in the starts above us that
-          make astronomy so much fun.
-        </h3>
+        <h2 className="Item__description--title">{title}</h2>
+        <h3 className="Item__description--subtitle">{subtitle}</h3>
       </div>
     </div>
   );
