35be0003df4448d907fbfe2082711d2a358061e7
Alexis Araujo <ala1289@gmail.com>
2020-02-19 23:09:39 -0600

    [ADD] Se agrego normalize y componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,37 +2,39 @@ import React from 'react';
 import '../styles/components/Header.styl';
 import Logo from '../../resources/imgs/Logo.png';
 
-const Header = () => (
-  <div className="Header">
-    <figure className="Header__logoFigure">
-      <img src={Logo} alt="" />
-    </figure>
-    <nav className="Nav" id="Nav">
-      <ul className="Nav__list">
-        <li className="Nav_element">
-          <a href="/">TECHNOLOGY</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">IDEAS</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">LEADERSHIP</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">VIDEO</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">NEWS</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">FINANCE</a>
-        </li>
-        <li className="Nav_element">
-          <a href="/">ENTERTAIMENT</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <figure className="header__img">
+        <img src={Logo} alt="logo" />
+      </figure>
+      <nav className="nav" id="nav" alt="menu">
+        <ul className="nav__menu">
+          <li className="nav__menu--items">
+            <a href="/">TECHNOLOGY</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">IDEAS</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">LEADERSHIP</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">VIDEO</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">NEWS</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">FINANCE</a>
+          </li>
+          <li className="nav__menu--items">
+            <a href="/">ENTERTAIMENT</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
