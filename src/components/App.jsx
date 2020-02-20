de38c7f920b7a30e44edad9e938e974736f082e6
AryRosvall <ary.rosvall@gmail.com>
2020-02-19 20:58:04 -0600

    Adding header components style file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import '../styles/components/App.styl';
+import '../assets/styles/components/App.styl';
 
 const App = () => {
   return <Header />;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import '../assets/styles/components/Header.styl';
 
 const Header = () => {
-  return <header> </header>;
+  return (
+    <header>
+      <div className="header__logo">
+        <span>Deck</span>
+        <span>.</span>
+      </div>
+      <nav>
+        <ul>
+          <li>TECHNOLOGY</li>
+          <li>IDEAS</li>
+          <li>LEADERSHIP</li>
+          <li>VIDEO</li>
+          <li>NEWS</li>
+          <li>FINANCE</li>
+          <li>ENTERTAINMENT</li>
+        </ul>
+        <div htmlFor="toggle">&#9776;</div>
+        <input type="checkbox" id="toggle" />
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
