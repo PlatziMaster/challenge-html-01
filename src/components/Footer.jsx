1266570d2f0176cfaa88964485e82e73b2e4bf90
Jose Luis Campos Bautista <bautistaj20@hotmail.com>
2020-02-20 12:14:30 -0600

    Add contentand styles on footer and header.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import '../styles/components/Footer.styl';
 
-const Footer = () => <h1>Footer</h1>;
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <h3 className="Footer__item">Leadership video</h3>
+      <h3 className="Footer__item">New finance</h3>
+      <h3 className="Footer__item">Entertaiment</h3>
+      <div className="Footer__item">
+        <ul className="Footer__social">
+          <li>Follow us:</li>
+          <li>&#9776;</li>
+          <li>&#9776;</li>
+          <li>&#9776;</li>
+          <li>&#9776;</li>
+        </ul>
+        <div className="Footer__subscribe">
+          <input type="text" id="inputSubscribe" className="" />
+          <button type="button" className="Footer__btn">
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import '../styles/components/Header.styl';
 
-const Header = () => <h1>Header</h1>;
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <menu>
+          <li>
+            <img src="" alt="" />
+          </li>
+          <li>Technology</li>
+          <li>Ideas</li>
+          <li>Leadership</li>
+          <li>Video</li>
+          <li>News</li>
+          <li>Finance</li>
+          <li>Entertainment</li>
+        </menu>
+        <span>&#9776;</span>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
