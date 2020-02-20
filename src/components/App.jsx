17161f37d5f1ca713be3c69f497248b823c37d02
Alejandro Cortez <ziker@live.com.mx>
2020-02-19 21:08:37 -0600

    Added Footer component to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import '../assets/styles/components/App.styl';
 
-const App = () => {
-  return <Header />;
-};
+const App = () => (
+  <>
+    <Header />
+    <Footer />
+  </>
+);
 
 export default App;
