d76f306b40c364868ff3a1863e17802023b753bb
Jefferson Aguilar <jefferson@alunices.com>
2020-02-20 08:04:21 -0500

    Corrección de identación CSS

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,118 +10,195 @@ const Home = () => (
   <HomeLayout>
     <div className="container">
       <div className="item">
-        <img src={img1} alt="" />
-        <h2>Using Banners Will Increase Trade Show Traffic</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img1} alt="" />
+          <h2>1Using Banners Will Increase Trade Show Traffic</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img2} alt="" />
-        <h2>Search Engine Optimization And Advertising</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img2} alt="" />
+          <h2>2Using Banners Will Increase Trade Show Traffic</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img3} alt="" />
-        <h2>How to Write A Good Headline For Facebook Ads</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img3} alt="" />
+          <h2>3Search Engine Optimization And Advertising</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img4} alt="" />
-        <h2>
-          6 Powerful Tips For Creating Testimonials That Sell Your Products Fast
-        </h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>4How to Write A Good Headline For Facebook Ads</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img5} alt="" />
-        <h2>15 Tips To Increase Your Adwords Profits</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>
+            56 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img4} alt="" />
-        <h2>
-          6 Powerful Tips For Creating Testimonials That Sell Your Products Fast
-        </h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img1} alt="" />
+          <h2>615 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img5} alt="" />
-        <h2>15 Tips To Increase Your Adwords Profits</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            76 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img4} alt="" />
-        <h2>
-          6 Powerful Tips For Creating Testimonials That Sell Your Products Fast
-        </h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>815 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img5} alt="" />
-        <h2>15 Tips To Increase Your Adwords Profits</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            6 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img4} alt="" />
-        <h2>
-          6 Powerful Tips For Creating Testimonials That Sell Your Products Fast
-        </h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>15 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img5} alt="" />
-        <h2>15 Tips To Increase Your Adwords Profits</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            6 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img4} alt="" />
-        <h2>
-          6 Powerful Tips For Creating Testimonials That Sell Your Products Fast
-        </h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>15 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
       <div className="item">
-        <img src={img5} alt="" />
-        <h2>15 Tips To Increase Your Adwords Profits</h2>
-        <p>
-          There is a lot of exciting stuff going on in the stars above us that
-          makes astronomy so much fun.
-        </p>
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            6 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>15 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            6 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <img src={img5} alt="" />
+          <h2>15 Tips To Increase Your Adwords Profits</h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content">
+          <img src={img4} alt="" />
+          <h2>
+            6 Powerful Tips For Creating Testimonials That Sell Your Products
+            Fast
+          </h2>
+          <p>
+            There is a lot of exciting stuff going on in the stars above us that
+            makes astronomy so much fun.
+          </p>
+        </div>
       </div>
     </div>
   </HomeLayout>
