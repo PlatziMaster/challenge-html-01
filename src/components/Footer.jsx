9df2df229fcf1952f48c8cc1467b5a766fbdffb5
IsmaelJdz <ismael_br7@hotmail.com>
2020-02-19 18:49:31 -0600

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,36 +8,34 @@ const Footer = () => {
         <p>2018 Deck</p>
         <p>Component based UI Kit</p>
       </div>
-      <div className="Footer__Reverse_Menu_Search">
-        <div className="Footer__Menu">
-          <ul>
-            <li>TECHNOLOGY</li>
-            <li>IDEAS</li>
-            <li>LEADERSHIP</li>
-            <li>ENTERTAINMENT</li>
-            <li>NEWS</li>
-            <li>FINANCE</li>
-            <li>VIDEO</li>
-          </ul>
+      <div className="Footer__Menu second">
+        <ul>
+          <li>TECHNOLOGY</li>
+          <li>IDEAS</li>
+          <li>LEADERSHIP</li>
+          <li>ENTERTAINMENT</li>
+          <li>NEWS</li>
+          <li>FINANCE</li>
+          <li>VIDEO</li>
+        </ul>
+      </div>
+      <div className="Footer__Newsletter">
+        <div className="Footer__Social social">
+          <span className="Footer__text_icon redes">Follow us:</span>
+          <span className="icon-linkedin redes" />
+          <span className="icon-github redes" />
+          <span className="icon-twitter redes" />
+          <span className="icon-facebook redes" />
         </div>
-        <div className="Footer__Newsletter">
-          <div className="Footer__Social">
-            <span className="Footer__text_icon redes">Follow us:</span>
-            <span className="icon-linkedin redes" />
-            <span className="icon-github redes" />
-            <span className="icon-twitter redes" />
-            <span className="icon-facebook redes" />
-          </div>
-          <div className="Footer__Search">
-            <input
-              type="text"
-              className="Footer__input"
-              placeholder="Your E-mail"
-            />
-            <button type="button" className="Footer__subs">
-              SUSCRIBETE
-            </button>
-          </div>
+        <div className="Footer__Search">
+          <input
+            type="text"
+            className="Footer__input"
+            placeholder="Your E-mail"
+          />
+          <button type="button" className="Footer__subs">
+            SUSCRIBETE
+          </button>
         </div>
       </div>
     </div>
