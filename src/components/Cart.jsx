eb660a15eef15d91dfd43d9e7a272a5b42dc990b
Heladito <nicolasestevanprieto@gmail.com>
2020-02-21 17:02:23 -0500

    Complete design of BaseComponent

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Image from '../../resources/imgs/dose-media-344938.png';
+import '../styles/components/Cart.styl';
 
 const Cart = () => {
   return (
-    <div className="targer">
-      <div className="target-imgContainer">
-        <img src={Image} alt="" className="target-imgContainer__img" />
-        <h1 className="target-imgContainer__redTitle">g</h1>
+    <div className="cart">
+      <div className="cart-imgContainer">
+        <div className="cart-imgContainer__redTitleContainer">
+          <h1 className="imgContainer-redTitleContainer__redTitle">
+            EVO-DE-EVOlucion
+          </h1>
+        </div>
+        <img src={Image} alt="" className="cart-imgContainer__img" />
       </div>
-      <div className="targer-description">
-        <h2 className="targer-description__subtitle">g</h2>
-        <p clasName="targer-description__paragraph">
+      <div className="cart-description">
+        <h2 className="cart-description__subtitle">Evolucionas al verlo</h2>
+        <p className="cart-description__paragraph">
           El electrón tiene una carga eléctrica de −1,6 × 10−19 C y una masa de
           9,1 × 10-31 kg , que es aproximadamente 1.800 veces menor que la masa
           del protón o a la del neutrón. El electrón es una partícula elemental
