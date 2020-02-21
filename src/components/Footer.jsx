92bb9ad51a55969912fb3b3c5198809ef16e11c1
Rulo_code <drew.lizcano@gmail.com>
2020-02-21 00:51:16 -0500

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import '../styles/components/Footer.styl';
 const Footer = () => {
   return (
     <div className="Footer">
-      <div>
+      <div className="Footer__logoContainer">
         <div className="Footer__logo">
           Deck
           <p>.</p>
@@ -18,16 +18,16 @@ const Footer = () => {
       </div>
       <div className="Footer__navbar">
         <ul>
-          <li>Techology</li>
-          <li>Leadership</li>
-          <li>News</li>
-          <li>Entertainment</li>
-          <li>Ideas</li>
-          <li>Video</li>
-          <li>Finance</li>
+          <li className="technology">Technology</li>
+          <li className="leadership">Leadership</li>
+          <li className="news">News</li>
+          <li className="entertainment">Entertainment</li>
+          <li className="ideas">Ideas</li>
+          <li className="video">Video</li>
+          <li className="finance">Finance</li>
         </ul>
       </div>
-      <div className="Footer-followContainer">
+      <div className="Footer__followContainer">
         <div className="Footer__follow">
           <p>Follow us:</p>
           <i className="fab fa-instagram" />
@@ -36,7 +36,7 @@ const Footer = () => {
           <i className="fab fa-facebook-f" />
         </div>
 
-        <div>
+        <div className="subscribeContaineer">
           <input className="input" type="text" placeholder="Your E-mail" />
           <input className="submit" type="submit" value="SUBSCRIBE" />
         </div>
