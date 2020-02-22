7e71dc00ebd3466bba07fd02e545c6ae11765e80
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-02-22 15:21:09 -0600

    header

--- resources/template/script.js
+++ resources/template/script.js
@@ -1,35 +1,35 @@
-function resizeGridItem(item) {
-  grid = document.getElementsByClassName('container')[0];
-  rowHeight = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
-  );
-  rowGap = parseInt(
-    window.getComputedStyle(grid).getPropertyValue('grid-row-gap')
-  );
-  rowSpan = Math.ceil(
-    (item.querySelector('.card--content').getBoundingClientRect().height +
-      rowGap) /
-      (rowHeight + rowGap)
-  );
-  item.style.gridRowEnd = 'span ' + rowSpan;
-}
+// function resizeGridItem(item) {
+//   grid = document.getElementsByClassName('container')[0];
+//   rowHeight = parseInt(
+//     window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')
+//   );
+//   rowGap = parseInt(
+//     window.getComputedStyle(grid).getPropertyValue('grid-row-gap')
+//   );
+//   rowSpan = Math.ceil(
+//     (item.querySelector('.card--content').getBoundingClientRect().height +
+//       rowGap) /
+//       (rowHeight + rowGap)
+//   );
+//   item.style.gridRowEnd = 'span ' + rowSpan;
+// }
 
-function resizeAllGridItems() {
-  allItems = document.getElementsByClassName('card');
-  for (x = 0; x < allItems.length; x++) {
-    resizeGridItem(allItems[x]);
-  }
-}
+// function resizeAllGridItems() {
+//   allItems = document.getElementsByClassName('card');
+//   for (x = 0; x < allItems.length; x++) {
+//     resizeGridItem(allItems[x]);
+//   }
+// }
 
-function resizeInstance(instance) {
-  item = instance.elements[0];
-  resizeGridItem(item);
-}
+// function resizeInstance(instance) {
+//   item = instance.elements[0];
+//   resizeGridItem(item);
+// }
 
-window.onload = resizeAllGridItems();
-window.addEventListener('resize', resizeAllGridItems);
+// window.onload = resizeAllGridItems();
+// window.addEventListener('resize', resizeAllGridItems);
 
-allItems = document.getElementsByClassName('card');
-for (x = 0; x < allItems.length; x++) {
-  imagesLoaded(allItems[x], resizeInstance);
-}
+// allItems = document.getElementsByClassName('card');
+// for (x = 0; x < allItems.length; x++) {
+//   imagesLoaded(allItems[x], resizeInstance);
+// }
