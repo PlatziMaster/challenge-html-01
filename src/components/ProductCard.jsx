061baa15c7cd6022a8d1dee77199d8820738ec5e
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-02-23 16:07:26 -0500

    tag relocated

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,13 +6,13 @@ function ProductCard({ product }) {
   return (
     <div className="ProductCard">
       <div className="ProductCard__head">
-        {product.tags.map((tag) => (
-          <span className="ProductCard__tag" key={product.key}>
-            {tag}
-          </span>
-        ))}
         <figure className="ProductCard__imageFigure">
           <img className="ProductCard__image" src={product.imgSrc} alt="" />
+          {product.tags.map((tag) => (
+            <span className="imageFigure__tag" key={product.key}>
+              {tag}
+            </span>
+          ))}
         </figure>
       </div>
       <h1 className="ProductCard__title">{product.title}</h1>
