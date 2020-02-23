2303d1a696fac9b4a1b8be84dd13b4a601a495d7
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-22 22:58:43 -0600

    Add Footer and social icons of type svg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/components/App.styl';
 import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import twitter from "../../resources/imgs/twitter.svg";
+import instagram from "../../resources/imgs/instagram.svg";
+import pinterest from "../../resources/imgs/pinterest.svg";
+import facebook from "../../resources/imgs/facebook.svg";
+import '../styles/components/Footer.styl';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-branding">
+        <p className="footer-title">
+          Deck
+          <span>.</span>
+        </p>
+        <p>2018 Deck</p>
+        <p>Component based UI Kit</p>
+      </div>
+      <div className="footer-subscribe">
+        <div className="subscribe-box">
+          <input type="text" placeholder="Your E-mail" id="subscribe" />
+          <label htmlFor="subscribe">Suscribirse</label>
+        </div>
+      </div>
+      <div className="footer-socials">
+        <p>Follow us</p>
+        <img src={instagram} alt="" />
+        <img src={pinterest} alt="" />
+        <img src={twitter} alt="" />
+        <img src={facebook} alt="" />
+      </div>
+      <p>technology</p>
+      <p>ideas</p>
+      <p>entretaiment</p>
+      <p>ladership</p>
+      <p>video</p>
+      <p>news</p>
+      <p>finance</p>
+    </footer>
+  );
+};
+
+export default Footer;
