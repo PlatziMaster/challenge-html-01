159d6b428ff7d9f1de3ed63fdab2b479d886490d
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-23 17:36:40 -0500

    [base] Header initial, it require yet job

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Logo from './Logo';
+import Nav from './Nav';
+import MenuIcon from './MenuIcon';
 
 const Header = () => {
   return (
     <header>
       <Logo />
+      <Nav />
+      <MenuIcon />
     </header>
   );
 };
