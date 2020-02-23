cbeb66301c06c79afb07a7823d830ad792fd144c
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-23 10:51:04 -0500

    [base] Menu icon with horizontal style bars.

---
+++ src/components/MenuIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MenuIcon = () => {
+  return (
+    <div className='menu-icon'>
+      <div className='menu-icon__bar' />
+      <div className='menu-icon__bar--middle' />
+      <div className='menu-icon__bar' />
+    </div>
+  );
+};
+
+export default MenuIcon;
