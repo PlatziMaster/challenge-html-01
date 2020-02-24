19ee95f3fc400375ef0cc967c32ed43c8dd97ba2
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-02-23 18:23:53 -0600

    Add Footer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
       {children}
+
+      <Footer />
     </div>
   );
 };
