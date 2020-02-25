cd87d3461bcfa44a90f2230f1dfd8e78e02e3f76
luisrdz5 <luis@luisrodriguez.com>
2020-02-24 22:36:34 -0600

    finished the react modules

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 const Home = () => {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 };
