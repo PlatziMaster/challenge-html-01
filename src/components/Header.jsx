0336945df5a9850e02ead1f71460fdddb08a2dc0
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-24 21:51:33 -0500

    [fix] fix 0 - Header in mobile first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from './Logo';
 import Nav from './Nav';
 import MenuIcon from './MenuIcon';
 
 const Header = () => {
+  const doVisibleNav = () => {
+    const $headerNav = document.getElementById('header__nav');
+    $headerNav.classList.toggle('header__nav--visible');
+    $headerNav.classList.toggle('header__nav--hidden');
+  };
+
   return (
-    <header>
-      <Logo />
-      <Nav />
-      <MenuIcon />
+    <header className='header'>
+      <div className='header__container-icons'>
+        <div className='header__logo'>
+          <Link to='/'>
+            <Logo />
+          </Link>
+        </div>
+
+        <button
+          type='button'
+          className='header__menu-icon'
+          onClick={doVisibleNav}
+        >
+          <MenuIcon />
+        </button>
+      </div>
+
+      <button
+        type='button'
+        id='header__nav'
+        className='header__nav header__nav--hidden'
+        onClick={doVisibleNav}
+      >
+        <Nav />
+      </button>
     </header>
   );
 };
