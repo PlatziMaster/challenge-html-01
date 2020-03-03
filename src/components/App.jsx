5a45b462c50af2157a46aeeb2d64adbc0dcbbdb5
Luis Caraballo <luisred61@gmail.com>
2020-03-03 14:16:00 -0500

    [ADD] Main section for posts created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import Posts from './Posts';
 import '../styles/components/App.styl';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Posts />
       <Footer />
     </>
   );
