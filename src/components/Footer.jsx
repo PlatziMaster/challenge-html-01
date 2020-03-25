e2e5c9fe0cf79b4b88e8baa4c5af41eb14a7d57b
luis_parra <luis.parra.pulido@gmail.com>
2020-03-25 17:23:00 -0500

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import '../styles/components/Footer.css';
 
 const Footer = () => (
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <footer className="footer-container">
+    <div className="footer-logo">
+      <h3>Deck.</h3>
+
+      <p>
+        © 2018 Deck
+        <br />
+        Component based UI Kit
+      </p>
+    </div>
+    <div className="footer-categories">
+      <a href="/">TECHNOLOGY</a>
+      <a href="/">IDEAS</a>
+      <a href="/">LEADERSHIP</a>
+      <a href="/">VIDEO</a>
+      <a href="/">NEWS</a>
+      <a href="/">FINANCE</a>
+      <a href="/">ENTERTAINMENT</a>
+    </div>
+    <div className="footer-social">
+      <div className="footer-social__media">
+        <p>Follow us:</p>
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </div>
+      <div className="footer-social__subscribe">
+        <input type="email" placeholder="Your E-mail" />
+        <button type="button">SUBSCRIBE</button>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
