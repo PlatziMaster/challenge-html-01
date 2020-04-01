88cc3d48bf66c4272f9f0055de4b3736f7e0dbc9
Andres Campuzano <campuxano88@gmail.com>
2020-04-01 08:51:00 -0500

    Deleted red marks in ArticleItem.js.

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -17,14 +17,14 @@ class ArticleItem extends Component {
         {this.state.size === true ? (
           <div className="articles--item large">
             <img src={this.props.image} alt={this.props.image} />
-            <span className="articles--item-box">Test label</span>
+            {/* <span className="articles--item-box">Test label</span> */}
             <h2>{this.props.title}</h2>
             <p>{this.props.text}</p>
           </div>
         ) : (
           <div className="articles--item short">
             <img src={this.props.image} alt={this.props.image} />
-            <span className="articles--item-box">Test label</span>
+            {/* <span className="articles--item-box">Test label</span> */}
             <h2>{this.props.title}</h2>
             <p>{this.props.text}</p>
           </div>
