d690f52cefbba202fbd6bbb5cee440f26d06df1f
Andres Campuzano <campuxano88@gmail.com>
2020-04-01 10:01:14 -0500

    fixing eslint.

--- src/components/ArticleItem.js
+++ src/components/ArticleItem.js
@@ -1,37 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/components/ArticleItem.css';
 
-class ArticleItem extends Component {
-  // ({ image, title, text, sizeGrid })
-  constructor(props) {
-    super(props);
-    this.state = {
-      size: props.sizeGrid,
-    };
-  }
-
-  // large and short are values given in ArticleItem.css
-  render() {
-    return (
-      <>
-        {this.state.size === true ? (
-          <div className="articles--item large">
-            <img src={this.props.image} alt={this.props.image} />
-            {/* <span className="articles--item-box">Test label</span> */}
-            <h2>{this.props.title}</h2>
-            <p>{this.props.text}</p>
-          </div>
-        ) : (
-          <div className="articles--item short">
-            <img src={this.props.image} alt={this.props.image} />
-            {/* <span className="articles--item-box">Test label</span> */}
-            <h2>{this.props.title}</h2>
-            <p>{this.props.text}</p>
-          </div>
-        )}
-      </>
-    );
-  }
-}
+const ArticleItem = ({ image, title, text, sizeGrid }) => {
+  return (
+    <>
+      {sizeGrid === true ? (
+        <div className="articles--item large">
+          <img src={image} alt={image} />
+          {/* <span className="articles--item-box">Test label</span> */}
+          <h2>{title}</h2>
+          <p>{text}</p>
+        </div>
+      ) : (
+        <div className="articles--item short">
+          <img src={image} alt={image} />
+          {/* <span className="articles--item-box">Test label</span> */}
+          <h2>{title}</h2>
+          <p>{text}</p>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default ArticleItem;
+
+// class ArticleItem extends Component {
+//   // ({ image, title, text, sizeGrid })
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       size: props.sizeGrid,
+//     };
+//   }
+
+// large and short are values given in ArticleItem.css
+//   render() {
+//     return (
+//       <>
+//         {this.state.size === true ? (
+//           <div className="articles--item large">
+//             <img src={this.props.image} alt={this.props.image} />
+//             {/* <span className="articles--item-box">Test label</span> */}
+//             <h2>{this.props.title}</h2>
+//             <p>{this.props.text}</p>
+//           </div>
+//         ) : (
+//           <div className="articles--item short">
+//             <img src={this.props.image} alt={this.props.image} />
+//             {/* <span className="articles--item-box">Test label</span> */}
+//             <h2>{this.props.title}</h2>
+//             <p>{this.props.text}</p>
+//           </div>
+//         )}
+//       </>
+//     );
+//   }
+// }
