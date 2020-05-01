59ecadad4595e531ffa3c0129b26ba5193c3cea4
Miguel Angel Martelo Quiroz <miguelmartelo@seven4n.com>
2020-05-01 14:04:29 -0500

    Styling content

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,138 +1,103 @@
 import React from 'react';
+import '../styles/components/Content.css';
 
 function Content() {
   return (
-    <div className="content">
+    <div className="cards__container">
       <div className="card">
-        <img
-          src="../../resources/imgs/camara.png"
-          alt="Camara"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/camara.png" alt="Camara" />
         <span className="card__category">Technology</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Using Banner Stands To Increase Trade Show Traffic
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
-      <div className="card">
-        <img
-          src="../../resources/imgs/phone.png"
-          alt="Phone"
-          className="card__img"
-        />
+      <div className="card ">
+        <img src="../../resources/imgs/phone.png" alt="Phone" />
         <span className="card__category">Technology</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Search Engine Optimization And Advertising
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/watch.png"
-          alt="Watch"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/watch.png" alt="Watch" />
         <span className="card__category">Finance</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Writing A Good Headline For Your Advertisement
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/dron.png"
-          alt="Dron"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/dron.png" alt="Dron" />
         <span className="card__category">Technology</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           6 Powerful Tips To Creating Testimonials That Sell Your Products Fast
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/tablet.png"
-          alt="Tablet"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/tablet.png" alt="Tablet" />
         <span className="card__category">Entertainment</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           15 Tips To Increase Your Adwords Profits
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/camara.png"
-          alt="Camara"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/camara.png" alt="Camara" />
         <span className="card__category">Technology</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Using Banner Stands To Increase Trade Show Traffic
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/tablet.png"
-          alt="Tablet"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/tablet.png" alt="Tablet" />
         <span className="card__category">Entertainment</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           15 Tips To Increase Your Adwords Profits
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/watch.png"
-          alt="Watch"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/watch.png" alt="Watch" />
         <span className="card__category">Finance</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Writing A Good Headline For Your Advertisement
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
         </p>
       </div>
       <div className="card">
-        <img
-          src="../../resources/imgs/phone.png"
-          alt="Phone"
-          className="card__img"
-        />
+        <img src="../../resources/imgs/phone.png" alt="Phone" />
         <span className="card__category">Technology</span>
-        <h3 className="card__title">
+        <h1 className="card__title">
           Search Engine Optimization And Advertising
-        </h3>
+        </h1>
         <p className="card__description">
           There is a lot of exciting stuff going on in the starts above us that
           make astronomy so much fun.
