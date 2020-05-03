5ff28d598d74fd9b56d60cb4d97bf7841882dbb4
camilo <velandiacamilo356@gmail.com>
2020-05-03 18:08:34 -0500

    footer end

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import '../styles/components/footer.styl';
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <div className="footer__brand">
+        <h1>
+          {' '}
+          Deck
+          <span>.</span>
+        </h1>
+        <p>2018 Deck</p>
+        <p>Component based UI Kit</p>
+      </div>
+      <div className="footer__section">
+        <ul>
+          <li>TECNOLOGY</li>
+          <li>IDEAS</li>
+          <li>LEADERSHIP</li>
+          <li>VIDEO</li>
+          <li>NEWS</li>
+          <li>FINANCE</li>
+          <li>ENTERTAINMENT</li>
+        </ul>
+      </div>
+      <div className="footer__social">
+        <p>Follow us: . . . .</p>
+        <div className="footer__social-input">
+          <input type="text" placeholder="Your Email" />
+          <button type="submit">SUBSCRIBE</button>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
