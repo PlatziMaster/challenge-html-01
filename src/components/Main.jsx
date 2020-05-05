e656d8164fc6d58186f95634ab26ec33f102f646
camilo <velandiacamilo356@gmail.com>
2020-05-04 20:22:21 -0500

    challenge solved

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,126 @@
 import React from 'react';
+import '../styles/components/main.styl';
 
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <main>
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/camara.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img
+            className="image"
+            src="../../resources/imgs/celular.png"
+            alt=""
+          />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/reloj.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/dron.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/tablet.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/camara.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/tablet.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img className="image" src="../../resources/imgs/reloj.png" alt="" />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+
+      <div className="item">
+        <div className="item__image">
+          <img
+            className="image"
+            src="../../resources/imgs/celular.png"
+            alt=""
+          />
+          <span className="image__category">Technology</span>
+        </div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste libero
+          quos nemo, inventore porro eligendi impedit?
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
