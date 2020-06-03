953713487e5e38f4a50b49179f71cdfb5cda0f2a
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-02 21:33:29 -0500

    init Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/components/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        Deck
+        <span>.</span>
+      </div>
+      <nav className="header__nav">
+        <ul>
+          <li>Technology</li>
+          <li>Ideas</li>
+          <li>Leadership</li>
+          <li>Vide</li>
+          <li>News</li>
+          <li>Finance</li>
+          <li>Entertainment</li>
+        </ul>
+      </nav>
+      <div className="header__burger">burger</div>
+    </header>
+  );
+};
+
+Header.propTypes = {};
+
+export default Header;
