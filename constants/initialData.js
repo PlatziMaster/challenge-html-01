84cf7ed6e8c568bcc39c1d6c92c2f8b7b51928e6
Mariangelica Useche <mariangelica.useche@gmail.com>
2020-06-11 01:22:34 -0500

    Add initialData

---
+++ constants/initialData.js
@@ -0,0 +1,113 @@
+const initialData = [
+  {
+    id: 1,
+    level: 2,
+    category: 'Technology',
+    title: 'Using Banners Will Increase Trade Show Traffic',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/dose-media-344938.png',
+      alt: 'A camera',
+    },
+  },
+  {
+    id: 2,
+    level: 1,
+    category: 'Technology',
+    title: 'Search Engine Optimization And Advertising',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/aidan-hancock-455440.png',
+      alt: 'Iphone 11 pro max',
+    },
+  },
+  {
+    id: 3,
+    level: 2,
+    category: 'Finance',
+    title: 'How to Write A Good Headline For Facebook Ads',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/crew-22235.png',
+      alt: 'Admin your time',
+    },
+  },
+  {
+    id: 4,
+    level: 1,
+    category: 'Technology',
+    title:
+      '6 Powerful Tips For Creating Testimonials That Sell Your Products Fast',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/annie-spratt-213059.png',
+      alt: 'autonomous drone',
+    },
+  },
+  {
+    id: 5,
+    level: 2,
+    category: 'Entertainment',
+    title: '15 Tips To Increase Your Adwords Profits',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/jeff-sheldon-264920.png',
+      alt: 'battery tablet',
+    },
+  },
+  {
+    id: 6,
+    level: 2,
+    category: 'Technology',
+    title: 'Using Banners Will Increase Trade Show Traffic',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/dose-media-344938.png',
+      alt: 'A camera',
+    },
+  },
+  {
+    id: 7,
+    level: 2,
+    category: 'Entertainment',
+    title: '15 Tips To Increase Your Adwords Profits',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/jeff-sheldon-264920.png',
+      alt: 'battery tablet',
+    },
+  },
+  {
+    id: 8,
+    level: 2,
+    category: 'Finance',
+    title: 'How to Write A Good Headline For Facebook Ads',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/crew-22235.png',
+      alt: 'Admin your time',
+    },
+  },
+  {
+    id: 9,
+    level: 1,
+    category: 'Technology',
+    title: 'Search Engine Optimization And Advertising',
+    description:
+      'There is a lot of exciting stuff going on in the stars above us that makes astronomy so much fun.',
+    imageInfo: {
+      src: 'resources/imgs/aidan-hancock-455440.png',
+      alt: 'Iphone 11 pro max',
+    },
+  },
+];
+
+export default initialData;
